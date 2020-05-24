e5a28157c6da5a8fa695bff224e49ccd32fd2b74
hustlong <biglongcc@gmail.com>
2020-05-24 20:46:12 +0800

    resize handler

--- src/views/mixin/ResizeHandler.js
+++ src/views/mixin/ResizeHandler.js
@@ -1,5 +1,53 @@
+// import store from '@/store'
+//
+// const { body } = document
+// const WIDTH = 1024
+// const RATIO = 3
+//
+// export default {
+//     watch: {
+//         // $route(route) {
+//         //     if (this.device === 'mobile' && this.sidebar.opened) {
+//         //         store.dispatch('CloseSideBar', { withoutAnimation: false })
+//         //     }
+//         // }
+//     },
+//     beforeMount() {
+//         window.addEventListener('resize', this.resizeHandler)
+//         this.resizeHandler()
+//     },
+//     mounted() {
+//         // const isMobile = this.isMobile()
+//         // if (isMobile) {
+//         //     store.dispatch('ToggleDevice', 'mobile')
+//         //     store.dispatch('CloseSideBar', { withoutAnimation: true })
+//         // }
+//     },
+//     methods: {
+//         isMobile() {
+//             const rect = body.getBoundingClientRect()
+//             return rect.width - RATIO < WIDTH
+//         },
+//         resizeHandler() {
+//             if (!document.hidden) {
+//                 const isMobile = this.isMobile()
+//                 const originSate = this.device === 'mobile'
+//
+//                 if (isMobile !== originSate) {
+//                     console.log("尺寸改变：isMobile=", isMobile)
+//                     store.dispatch('ToggleDevice', isMobile ? 'mobile' : 'desktop')
+//                     if (isMobile) {
+//                         store.dispatch('CloseSideBar', { withoutAnimation: true })
+//                     } else {
+//                         store.dispatch('OpenSideBar', { withoutAnimation: true })
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// }
+
 import store from '@/store'
-import el from "element-ui/src/locale/lang/el";
 
 const { body } = document
 const WIDTH = 1024
@@ -7,22 +55,23 @@ const RATIO = 3
 
 export default {
     watch: {
-        // $route(route) {
-        //     if (this.device === 'mobile' && this.sidebar.opened) {
-        //         store.dispatch('CloseSideBar', { withoutAnimation: false })
-        //     }
-        // }
+        $route(route) {
+            if (this.device === 'mobile' && this.sidebar.opened) {
+                store.dispatch('CloseSideBar', { withoutAnimation: false })
+            }
+        }
     },
     beforeMount() {
         window.addEventListener('resize', this.resizeHandler)
-        this.resizeHandler()
     },
     mounted() {
-        // const isMobile = this.isMobile()
-        // if (isMobile) {
-        //     store.dispatch('ToggleDevice', 'mobile')
-        //     store.dispatch('CloseSideBar', { withoutAnimation: true })
-        // }
+        const isMobile = this.isMobile()
+        store.dispatch('ToggleDevice', isMobile ? 'mobile' : 'desktop')
+        if (isMobile) {
+            store.dispatch('CloseSideBar', { withoutAnimation: true })
+        } else {
+            store.dispatch('OpenSideBar', { withoutAnimation: true })
+        }
     },
     methods: {
         isMobile() {
@@ -32,16 +81,11 @@ export default {
         resizeHandler() {
             if (!document.hidden) {
                 const isMobile = this.isMobile()
-                const originSate = this.device === 'mobile'
-
-                if (isMobile !== originSate) {
-                    console.log("尺寸改变：isMobile=", isMobile)
-                    store.dispatch('ToggleDevice', isMobile ? 'mobile' : 'desktop')
-                    if (isMobile) {
-                        store.dispatch('CloseSideBar', { withoutAnimation: true })
-                    } else {
-                        store.dispatch('OpenSideBar', { withoutAnimation: true })
-                    }
+                store.dispatch('ToggleDevice', isMobile ? 'mobile' : 'desktop')
+                if (isMobile) {
+                    store.dispatch('CloseSideBar', { withoutAnimation: true })
+                } else {
+                    store.dispatch('OpenSideBar', { withoutAnimation: true })
                 }
             }
         }
